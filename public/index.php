f06b3d8a3e13a72b9e8b932b918d49560198ad27
Julio <julioperaza@gmail.com>
2023-07-10 18:16:55 -0600

    l

--- public/index.php
+++ public/index.php
@@ -10,11 +10,10 @@
 </head>
 
 <body>
-  <!--  Body Wrapper -->
+  <!-- Body Wrapper -->
   <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
     data-sidebar-position="fixed" data-header-position="fixed">
-    <div
-      class="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
+    <div class="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
       <div class="d-flex align-items-center justify-content-center w-100">
         <div class="row justify-content-center w-100">
           <div class="col-md-8 col-lg-6 col-xxl-3">
@@ -24,32 +23,28 @@
                   <img src="../assets/images/logos/para fondos claros (2).jpg" width="250" alt="">
                 </a>
                
-                <form method="POST">
-
-                <?php 
-require("mgnLogin.php");
-?>
+                <form method="POST" action="mgnLogin.php">
                   <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Correo</label>
+                    <label for="email" class="form-label">Correo</label>
                     <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp">
                   </div>
                   <div class="mb-4">
-                    <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+                    <label for="password" class="form-label">Contraseña</label>
                     <input type="password" class="form-control" id="password" name="password">
                   </div>
                   <div class="d-flex align-items-center justify-content-between mb-4">
                     <div class="form-check">
                       <input class="form-check-input primary" type="checkbox" value="" id="flexCheckChecked" checked>
                       <label class="form-check-label text-dark" for="flexCheckChecked">
-                        Recuerdame
+                        Recuérdame
                       </label>
                     </div>
-                    <a class="text-primary fw-bold" href="./index.html">Olvido su contraseña ?</a>
+                    <a class="text-primary fw-bold" href="./index.html">¿Olvidó su contraseña?</a>
                   </div>
                   <button type="submit" name="login" id="login" class="btn btn-primary text-primary fw-bold ms-2">Ingresar</button>
                  
                   <div class="d-flex align-items-center justify-content-center">
-                    <p class="fs-4 mb-0 fw-bold">No tiene cuenta aún?</p>
+                    <p class="fs-4 mb-0 fw-bold">¿No tiene cuenta aún?</p>
                    
                    
                   </div>
